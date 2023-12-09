f6dfc117d2bf1f05bd9328c9e9c439572105a8f2
faroukm2002 <faroukm238@gamil.com>
2023-12-10 01:24:39 +0200

    .

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
 import { bootstrap } from "./src/bootstrap.js";
 import morgan from "morgan";
-const app = express();
 import cors from 'cors'
+
+const app = express();
 
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
 app.use(morgan("dev"));
 app.use(express.static('uploads'))
+app.use(cors());
+
 import dotenv from "dotenv";
 
 dotenv.config();
