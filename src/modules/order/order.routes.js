2bd012b3cbf1a7cb833585075edad8e4d303f90a
faroukm2002 <faroukm238@gamil.com>
2024-12-09 18:34:49 +0200

    s

--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -5,7 +5,7 @@ import { allowedto, protectRoutes } from "../auth/auth.controller.js";
 const orderRouter = express.Router();
 
 // Webhook route to handle Stripe events
-orderRouter.post('/webhook', express.raw({ type: 'application/json' }), order.createOnlineOrder);  // Raw body parsing for Stripe webhook
+// orderRouter.post('/webhook', express.raw({ type: 'application/json' }), order.createOnlineOrder);  // Raw body parsing for Stripe webhook
 
 // Checkout and Order routes
 orderRouter.post('/checkout/:id', protectRoutes, allowedto('user'), order.createCheckOutSession);
